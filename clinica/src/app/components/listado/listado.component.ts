46ce9da9755cb8329b0c0caebf05f11ad4f43c34
unknown <brunogoffredo22@gmail.com>
2022-06-16 18:52:44 -0300

    Correije errores visuales en login y agrega excel

--- clinica/src/app/components/listado/listado.component.ts
+++ clinica/src/app/components/listado/listado.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FirestoreService } from 'src/app/services/firestore.service';
-
+import * as FileSaver from 'file-saver';
 import { Usuario } from 'src/app/models/usuario';
 
 
@@ -25,6 +25,27 @@ export class ListadoComponent implements OnInit {
       })
       this.cargando = false;
     })
+  }
+
+  exportExcel() {
+    import("xlsx").then(xlsx => {
+      const worksheet = xlsx.utils.json_to_sheet(this.users); // Sale Data
+      const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+      const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+      this.saveAsExcelFile(excelBuffer, "users");
+    });
+  }
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE =
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+    let EXCEL_EXTENSION = ".xlsx";
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+    FileSaver.saveAs(
+      data,
+      fileName + "export" + new Date().getTime() + EXCEL_EXTENSION
+    );
   }
 
 }
